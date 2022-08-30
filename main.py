54e5f79db8b2d6621715359cf45e207481082308
rosg89 <rsuarezgalan@hotmail.com>
2022-08-30 11:57:17 -0300

    Se sube el proyecto

---
+++ main.py
@@ -0,0 +1,58 @@
+#Calculator
+from art import logo
+
+#Add
+def add(n1,n2):
+  return n1+n2
+
+#Subtract
+def subtract(n1,n2):
+  return n1-n2
+
+#Multiply
+def multiply(n1,n2):
+  return n1*n2
+
+#Divide
+def divide(n1,n2):
+  return n1/n2
+      
+#Diccionario
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+  #Pregunta el primer numero
+  num1 = float(input("What's the first number?: "))
+  
+  #For Loop
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+  
+  while should_continue:
+    
+    #Le dice que elija la operacion
+    operation_symbol = input("Pick an operation: ")
+    
+    #Pregunta el segundo numero
+    num2 = float(input("What's the next number?: "))
+    
+    calculation_function = operations[operation_symbol]
+    
+    answer = calculation_function(num1,num2)
+    
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
